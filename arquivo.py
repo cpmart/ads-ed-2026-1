5136e21c186c0f61e494d4cd579d3dece3b17a66
Cristiano Pires Martins <cpmart@gmail.com>
2026-03-09 10:44:12 -0300

    Material e lista 2

---
+++ arquivo.py
@@ -0,0 +1,4 @@
+with open('arquivo.txt', 'r') as arquivo:
+    #for linha in arquivo:
+    #    print(f"-> {linha.strip()}") 
+    print(type(arquivo))
--- ex1.py
+++ ex1.py
@@ -1,3 +1,6 @@
-print('mundo')
-
-print('terceiro commit')
+valor = input("Informe o valor: ")
+try:
+    valor = float(valor)
+    print(f"Dobro de {valor} é {valor*2}")
+except ValueError as erro:
+    print(f"Valor inválido: {erro}")
